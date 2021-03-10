284de48cc7006bc8ec5b9915b43d48d7a8cbbdc4
Taylor Treece <taylor.treece@protonmail.com>
2021-03-10 08:05:51 -0600

    commit tree f48a0650c76f2e6f24b1591255a5dd9e0e031a9a

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :session
+        set :sessions_secret, SecureRandom.hex(64)
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @item=params[:item]
+        session[:item]=params[:item]
+
+        erb :cart
+    end
 end
